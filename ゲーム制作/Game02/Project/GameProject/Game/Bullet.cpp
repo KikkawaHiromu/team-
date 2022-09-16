eb515216270440cc426dbd0b10905edfe38f4dc9
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-09-16 14:08:01 +0900

    a

---
+++ ゲーム制作/Game02/Project/GameProject/Game/Bullet.cpp
@@ -0,0 +1,34 @@
+#include "Bullet.h"
+
+Bullet::Bullet(const CVector2D& pos) :Base(eType_Bullet) {
+	m_img.Load("Image/ƒr[ƒ€3.png");
+	m_pos = pos;
+	m_rad = 12;
+	m_img.SetSize(50, 50);
+	m_img.SetCenter(16, 16);
+}
+
+void Bullet::Update()
+{
+	const int move_speed = 5;
+	m_pos.x -= move_speed;
+}
+
+void Bullet::Draw()
+{
+	m_img.SetPos(m_pos);
+	m_img.Draw();
+}
+
+void Bullet::COllsion(Base* b)
+{
+	switch (b->m_type) {
+	case eType_Enemy:
+		if (Base::CollisionCircle(this, b)) {
+			b->SetKill();
+			SetKill();
+			
+		}
+		break;
+	}
+}
