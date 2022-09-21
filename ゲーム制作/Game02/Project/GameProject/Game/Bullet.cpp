b93757003fd492e09363c0d5cbaa89d5c28e8539
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-09-21 14:54:43 +0900

    a

--- ゲーム制作/Game02/Project/GameProject/Game/Bullet.cpp
+++ ゲーム制作/Game02/Project/GameProject/Game/Bullet.cpp
@@ -5,12 +5,12 @@ Bullet::Bullet(const CVector2D& pos) :Base(eType_Bullet) {
 	m_pos = pos;
 	m_rad = 12;
 	m_img.SetSize(500, 50);
-	m_img.SetCenter(16, 16);
+	m_img.SetCenter(16,16);
 }
 
 void Bullet::Update()
 {
-	const int move_speed = 5;
+	const int move_speed = 9;
 	m_pos.x += move_speed;
 }
 
@@ -20,7 +20,7 @@ void Bullet::Draw()
 	m_img.Draw();
 }
 
-void Bullet::COllsion(Base* b)
+void Bullet::Collsion(Base* b)
 {
 	switch (b->m_type) {
 	case eType_Rock:
